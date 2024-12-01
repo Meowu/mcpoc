d4080f2ed2b87745cddb6bea694d7abb63b3b5cd
Meowu <474384902@qq.com>
2024-12-01 21:17:03 +0800

    add locallama pkg.

---
+++ packages/locallama/src/index.ts
@@ -0,0 +1,25 @@
+import path from 'node:path';
+import ollama from 'ollama'
+import fs from "fs/promises"
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+
+// Use `dirname` to get the __dirname equivalent
+const __dirname = dirname(__filename);
+
+const file = path.join(__dirname, 'image.png')
+const data = await fs.readFile(file)
+const image = data.toString('base64')
+
+const response = await ollama.chat({
+  model: 'llama3.2-vision',
+  messages: [{
+    role: 'user',
+    content: 'Understand the image, and return a short image name with ext .png',
+    images: [image]
+  }]
+})
+
+console.log(response)
